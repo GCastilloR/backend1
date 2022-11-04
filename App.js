6dd1adfe97a780b4e3110eae569f18620501c0ba
Gabriel Castillo <trodado@uninorte.edu.co>
2022-11-04 15:14:11 -0500

    pls

--- App.js
+++ App.js
@@ -3,37 +3,6 @@ const app = express();
 
 app.use(express.json())
 
-const data = {
-    users: [{
-        id:1,
-        nombre:"Gabriel Castillo",
-        username:"Yamielsun"},
-        {id:2,
-        nombre:"Hernan Castillo",
-        username:"Zeri"},
-        {id:3,
-        nombre:"Monica Rodado",
-        username:"Monysofy"
-        }
-    ],
-    products:[{
-        id:1,
-        nombre:"lapiz",
-        precio:10},
-        {id:2,
-        nombre:"borrador",
-        precio:8},
-        {id:3,
-        nombre:"sacapunta",
-        precio:5}
-    ],
-    compras:[{
-        id:1,
-        idUsuario:1,
-        idProducto:1}]
-}
-
-dotenv.config();
 
 const UserRouter = require("./routes/User");
 const tweetsRouter = require("./routes/tweets");
